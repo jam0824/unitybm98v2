2c515da8356fa5a6b5684da5137260655c67e4c4
m <aku_soku_zan_1bantai@hotmail.com>
2020-04-26 01:52:04 +0900

    音声再生の優先度をつけた

---
+++ vivration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vivration : MonoBehaviour
+{
+    public float VIVERATION_TIME = 0.1f;
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "MusicObject") {
+            StartCoroutine(Vivration(VIVERATION_TIME));
+        }
+    }
+
+    IEnumerator Vivration(float time) {
+        var activeController = OVRInput.GetActiveController();
+        OVRInput.SetControllerVibration(1, 1, activeController);
+        yield return new WaitForSeconds(time);
+        OVRInput.SetControllerVibration(0, 0, activeController);
+    }
+}
